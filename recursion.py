2e635e2dc00b62c1e17d922275c5b2cc098731c8
ninjaaung <ninja.aung02@gmail.com>
2020-04-08 17:22:49 -0700

    [Update] recursion.py, added recursive iteratve function

---
+++ recursion.py
@@ -0,0 +1,45 @@
+#!python
+
+def factorial(n):
+    """factorial(n) returns the product of the integers 1 through n for n >= 0,
+    otherwise raises ValueError for n < 0 or non-integer n"""
+
+
+
+    if not isinstance(n, int) or n < 0: # check if n is negative or not an integer (invalid input)
+        raise ValueError('factorial is undefined for n = {}'.format(n))
+
+    return factorial_iterative(n)
+
+
+def factorial_iterative(n):
+    factorial = 1
+    for factor in range(1,n+1):
+        factorial *= factor
+    return factorial
+
+
+
+def factorial_recursive(n):
+    # check if n is one of the base cases
+    if n == 0 or n == 1:
+        return 1
+    # check if n is an integer larger than the base cases
+    elif n > 1:
+        # call function recursively
+        return n * factorial_recursive(n - 1)
+
+
+def main():
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) == 1:
+        num = int(args[0])
+        result = factorial(num)
+        print('factorial({}) => {}'.format(num, result))
+    else:
+        print('Usage: {} number'.format(sys.argv[0]))
+
+
+if __name__ == '__main__':
+    main()
